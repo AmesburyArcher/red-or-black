ca3ab4dd24d6efd2ef5460a63d6af884d21314ea
Max C <smashmouthmax@gmail.com>
2022-09-17 14:49:39 -0700

    changed to Math.floor

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const page = document.querySelector("body");
 
 page.addEventListener("click", function () {
-  let val = Math.floor(Math.random() * 2);
+  let val = Math.round(Math.random());
   val === 1
     ? (page.style.backgroundColor = "black")
     : (page.style.backgroundColor = "red");
